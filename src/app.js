97d897e34225385fbdb31cd29a5a60d3c2250c4d
JMSBRN <zakhavai@gmail.com>
2023-01-17 09:38:34 +0300

    fix: upd. up command.

--- src/app.js
+++ src/app.js
@@ -7,35 +7,35 @@ export class App {
         this._curentPath = startDir; 
     }
 
-    start() {
+   async start() {
+        chdir(this._curentPath);
         const rl = createInterface({
             input: process.stdin,
             output: process.stdout
           });
-          process.chdir(this._curentPath);
-          rl.on('line', (line) => {
-            console.log(`You are currently in ${cwd()}\n`);
-           const [complection, lineIn]  = completer(line);
-           if (lineIn) {
-             switch (lineIn) {
-                case '.exit':
-                case '.quit':
-                case '.q':
-                    process.exit();
-                    break;
-                    case 'up':
-                        try {
-                          chdir('..');
-                        } catch (err) {
-                          console.error(`Operation failed: ${err}`);
-                        }
-                    break;
-                default:
-                    console.log('Invalid input');
-                    break;
+
+          while (true) {
+            const inputValues = await rl.question(`You are currently in ${cwd()}\n`);
+             const commands = inputValues.split(' ')[0];
+               switch (commands) {
+                  case '.exit':
+                  case '.quit':
+                  case '.q':
+                      process.exit();
+                      break;
+                      case 'up':
+                          try {
+                            chdir('..');
+                          } catch (err) {
+                            console.error(`Operation failed: ${err}`);
+                          }
+                      break;
+                  default:
+                      break;
+          }
              }
-           };
-          });
+           
+        
           
     }
 
