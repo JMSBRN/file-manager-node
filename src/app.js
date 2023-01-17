e14fb13341a6e4eaf6534d830c1d69719740a26d
JMSBRN <zakhavai@gmail.com>
2023-01-17 10:43:45 +0300

    fix: upd cd command.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
-import { completer } from "./utils/utils.js";
 import { createInterface } from 'readline/promises';
 import { cwd, chdir } from 'process';
 
 export class App {
     constructor (startDir) {
         this._curentPath = startDir; 
+    }
+    async up () {
+      try {
+        chdir('..');
+      } catch (err) {
+        console.error(`Operation failed: ${err}`);
+      }
+    }
+    async cd (arg) {
+      if (arg) {
+        try {
+          chdir(arg);
+        } catch (err) {
+          console.error(`Operation failed ${err}`);
+        }
+      } else {
+        console.log(`Invalid input`); 
+      }
     }
 
    async start() {
@@ -17,6 +34,7 @@ export class App {
           while (true) {
             const inputValues = await rl.question(`You are currently in ${cwd()}\n`);
              const commands = inputValues.split(' ')[0];
+             const arg = inputValues.split(' ').splice(1)[0];
                switch (commands) {
                   case '.exit':
                   case '.quit':
@@ -24,19 +42,16 @@ export class App {
                       process.exit();
                       break;
                       case 'up':
-                          try {
-                            chdir('..');
-                          } catch (err) {
-                            console.error(`Operation failed: ${err}`);
-                          }
+                      await this.up();
+                      break;
+                      case'cd': 
+                       await this.cd(arg);
                       break;
                   default:
                       break;
           }
              }
            
-        
-          
     }
 
 }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,8 +3,3 @@ export const toCapitalizeText = (text) => {
 }
 export const greeting  = (username) => console.log(`Welcome to the File Manager, ${toCapitalizeText(username)} !`);
 export const goodbye  = (username) => console.log(`Thank you for using File Manager, ${toCapitalizeText(username)} goodbye!`);
-export const completer = (line) =>  {
-  const completions = '.help .error .exit .quit .q'.split(' ');
-  const hits = completions.filter((c) => c.startsWith(line));
-  return [hits.length ? hits : completions, line];
-}
