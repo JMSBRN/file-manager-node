2e364a7a58eb41be854ef095b78ba44a15c10497
JMSBRN <zakhavai@gmail.com>
2023-01-24 13:28:39 +0300

    fix: upd. files module.

--- src/modules/files.js
+++ src/modules/files.js
@@ -12,17 +12,19 @@ import {
 import { utils } from '../modules/index.js';
 import { checkFileExistExtension } from "./utils.js";
 
-  export const readFile = (arg) => {
-    let chunk = "";
-    const rs = createReadStream(arg, { flags: "r" })
-      .on("readable", () => {
-        while (null !== (chunk = rs.read())) {
-          console.log(`${chunk}`);
-        }
-      })
-      .on("error", () => {
-        utils.opearationFailedMessage();
-      });
+  export const readFile = (src) => {
+    if (checkFileExistExtension(arg)) {
+      let chunk = "";
+      const rs = createReadStream(src, { flags: "r" })
+        .on("readable", () => {
+          while (null !== (chunk = rs.read())) {
+            console.log(`${chunk}`);
+          }
+        })
+        .on("error", () => {
+          utils.opearationFailedMessage();
+        });
+    }
   };
   export const addFile = async (src, content = "") => {
    if (checkFileExistExtension(src)) {
@@ -36,55 +38,63 @@ import { checkFileExistExtension } from "./utils.js";
     }
   };
   export const renameFile = async (src, dest) => {
-    readdirSync(cwd()).map((el) => {
-      if (el === src) {
-        if (dest) {
-          rename(src, dest);
-          console.log(utils.successMesages.renamed);
-        } else {
-          utils.opearationFailedMessage();;
+    if (checkFileExistExtension(src)) {
+      readdirSync(cwd()).map((el) => {
+        if (el === src) {
+          if (dest) {
+            rename(src, dest);
+            console.log(utils.successMesages.renamed);
+          } else {
+            utils.opearationFailedMessage();;
+          }
         }
-      }
-    });
+      });
+    }
   };
   export const copyFile = async (src, dest) => {
-    const rs = createReadStream(src, { flags: "r" }).on("error", () => {
-      utils.opearationFailedMessage();
-    });
-    if (dest) {
-      if (!existsSync(dest)) {
-       utils.createNewFolder(dest);
-        const ws = createWriteStream(dest).on("finish", () => {
-          console.log(utils.successMesages.copyed);
-        });
-        rs.pipe(ws);
+    if(checkFileExistExtension(src)) {
+      const rs = createReadStream(src, { flags: "r" }).on("error", () => {
+        utils.opearationFailedMessage();
+      });
+      if (dest) {
+        if (!existsSync(dest)) {
+         utils.createNewFolder(dest);
+          const ws = createWriteStream(dest).on("finish", () => {
+            console.log(utils.successMesages.copyed);
+          });
+          rs.pipe(ws);
+        } else {
+          utils.opearationFailedMessage();
+        }
+      }
+    }
+  };
+  export const moveFile = async (src, dest) => {
+    if (checkFileExistExtension(src)) {
+      const rs = createReadStream(src, { flags: "r" }).on("error", () => {
+        opearationFailedMessage();
+      });
+      if (dest) {
+        if (!existsSync(dest)) {
+          utils.createNewFolder(dest);
+          const ws = createWriteStream(dest).on("finish", () => {
+            unlink(src);
+            console.log(utils.successMesages.moved);
+          });
+          rs.pipe(ws);
+        }
       } else {
         utils.opearationFailedMessage();
       }
     }
   };
-  export const moveFile = async (src, dest) => {
-    const rs = createReadStream(src, { flags: "r" }).on("error", () => {
-      opearationFailedMessage();
-    });
-    if (dest) {
-      if (!existsSync(dest)) {
-        utils.createNewFolder(dest);
-        const ws = createWriteStream(dest).on("finish", () => {
-          unlink(src);
-          console.log(utils.successMesages.moved);
-        });
-        rs.pipe(ws);
-      }
-    } else {
-      utils.opearationFailedMessage();
-    }
-  };
   export const deleteFile = async (src) => {
-    unlink(src, (err) => {
-      if (err) {
-        utils.opearationFailedMessage();
-      }
-      console.log(utils.successMesages.deleted);
-    });
+    if(checkFileExistExtension(src)) {
+      unlink(src, (err) => {
+        if (err) {
+          utils.opearationFailedMessage();
+        }
+        console.log(utils.successMesages.deleted);
+      });
+    }
   };
